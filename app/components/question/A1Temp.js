1f157aeb2cabdfa92421282019db65af6e3d6d53
xlin <lxylinki@foxmail.com>
2019-06-06 18:06:07 +0800

    trivial

--- app/components/question/A1Temp.js
+++ app/components/question/A1Temp.js
@@ -16,23 +16,23 @@ export default class A1Temp extends Component {
 		        	<Text style={styles.quesText}>{'[A1题型] ' + this.props.question + ' (' + this.props.score + '分)'}</Text>
 		        </View>
 	        	
-	        	<View style={styles.optView}>
+	        	<View style={this.props.options.a_txt.length>0? styles.optView: {display: 'none'}}>
 	        		<Text style={styles.optText}>{'A. ' + this.props.options.a_txt}</Text>
 	        	</View>
 
-	        	<View style={styles.optView}>
+	        	<View style={this.props.options.b_txt.length>0? styles.optView: {display: 'none'}}>
 	        		<Text style={styles.optText}>{'B. ' + this.props.options.b_txt}</Text>
 	        	</View>
 	        	
-	        	<View style={styles.optView}>
+	        	<View style={this.props.options.c_txt.length>0? styles.optView: {display: 'none'}}>
 	        		<Text style={styles.optText}>{'C. ' + this.props.options.c_txt}</Text>
 	        	</View>
 	        	
-	        	<View style={styles.optView}>
+	        	<View style={this.props.options.d_txt.length>0? styles.optView: {display: 'none'}}>
 	        		<Text style={styles.optText}>{'D. ' + this.props.options.d_txt}</Text>
 	        	</View>
 	        	
-	        	<View style={styles.optView}>
+	        	<View style={this.props.options.e_txt.length>0? styles.optView: {display: 'none'}}>
 	        		<Text style={styles.optText}>{'E. ' + this.props.options.e_txt}</Text>
 	        	</View>
 	        	
